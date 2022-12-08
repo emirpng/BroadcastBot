86cdd6007838ff50eb538fe2e0b5216c67481e12
Emir <marveldarkbull@gmail.com>
2022-12-08 20:02:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMa
 
 import config
 from handlers.broadcast import broadcast
-from handlers.check_user import handle_user_status
+from handlers.check_group import handle_group_status
 from handlers.database import Database
 
 LOG_CHANNEL = config.LOG_CHANNEL
@@ -28,23 +28,23 @@ Bot = Client(
 
 @Bot.on_message(filters.group)
 async def _(bot, cmd):
-    await handle_user_status(bot, cmd)
+    await handle_group_status(bot, cmd)
 
 @Bot.on_message(filters.command("start") & filters.group)
 async def startgroup(client, message):
     # return
     chat_id = message.chat.id
-    if not await db.is_user_exist(chat_id):
+    if not await db.is_group_exist(chat_id):
         data = await client.get_me()
-        BOT_USERNAME = data.username
-        await db.add_user(chat_id)
+        BOT_GROUPNAME = data.groupname
+        await db.add_group(chat_id)
         if LOG_CHANNEL:
             await client.send_message(
                 LOG_CHANNEL,
-                f"#NEWUSER: \n\nNew User [{message.from_user.first_name}](tg://user?id={message.from_user.id}) started @{BOT_USERNAME} !!",
+                f"#NEWGROUP: \n\nNew Group [{message.from_group.first_name}](tg://group?id={message.from_group.id}) started @{BOT_GROUPNAME} !!",
             )
         else:
-            logging.info(f"#NewUser :- Name : {message.from_user.first_name} ID : {message.from_user.id}")
+            logging.info(f"#NewGroup :- Name : {message.from_group.first_name} ID : {message.from_group.id}")
     joinButton = InlineKeyboardMarkup(
         [
             [
@@ -60,14 +60,14 @@ async def startgroup(client, message):
 
 @Bot.on_message(filters.command("settings"))
 async def opensettings(bot, cmd):
-    user_id = cmd.from_user.id
+    group_id = cmd.from_group.id
     await cmd.reply_text(
-        f"`Here You Can Set Your Settings:`\n\nSuccessfully setted notifications to **{await db.get_notif(user_id)}**",
+        f"`Here You Can Set Your Settings:`\n\nSuccessfully setted notifications to **{await db.get_notif(group_id)}**",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        f"NOTIFICATION  {'🔔' if ((await db.get_notif(user_id)) is True) else '🔕'}",
+                        f"NOTIFICATION  {'🔔' if ((await db.get_notif(group_id)) is True) else '🔕'}",
                         callback_data="notifon",
                     )
                 ],
@@ -79,7 +79,7 @@ async def opensettings(bot, cmd):
 
 @Bot.on_message(filters.private & filters.command("broadcast"))
 async def broadcast_handler_open(_, m):
-    if m.from_user.id not in AUTH_USERS:
+    if m.from_group.id not in AUTH_USERS:
         await m.delete()
         return
     if m.reply_to_message is None:
@@ -90,45 +90,45 @@ async def broadcast_handler_open(_, m):
 
 @Bot.on_message(filters.private & filters.command("stats"))
 async def sts(c, m):
-    if m.from_user.id not in AUTH_USERS:
+    if m.from_group.id not in AUTH_USERS:
         await m.delete()
         return
     await m.reply_text(
-        text=f"**Total Users in Database 📂:** `{await db.total_users_count()}`\n\n**Total Users with Notification Enabled 🔔 :** `{await db.total_notif_users_count()}`",
+        text=f"**Total Groups in Database 📂:** `{await db.total_groups_count()}`\n\n**Total Groups with Notification Enabled 🔔 :** `{await db.total_notif_groups_count()}`",
         quote=True
     )
 
 
-@Bot.on_message(filters.private & filters.command("ban_user"))
+@Bot.on_message(filters.private & filters.command("ban_group"))
 async def ban(c, m):
-    if m.from_user.id not in AUTH_USERS:
+    if m.from_group.id not in AUTH_USERS:
         await m.delete()
         return
     if len(m.command) == 1:
         await m.reply_text(
-            f"Use this command to ban 🛑 any user from the bot 🤖.\n\nUsage:\n\n`/ban_user user_id ban_duration ban_reason`\n\nEg: `/ban_user 1234567 28 You misused me.`\n This will ban user with id `1234567` for `28` days for the reason `You misused me`.",
+            f"Use this command to ban 🛑 any group from the bot 🤖.\n\nUsage:\n\n`/ban_group group_id ban_duration ban_reason`\n\nEg: `/ban_group 1234567 28 You misused me.`\n This will ban group with id `1234567` for `28` days for the reason `You misused me`.",
             quote=True,
         )
         return
 
     try:
-        user_id = int(m.command[1])
+        group_id = int(m.command[1])
         ban_duration = int(m.command[2])
         ban_reason = " ".join(m.command[3:])
-        ban_log_text = f"Banning user {user_id} for {ban_duration} days for the reason {ban_reason}."
+        ban_log_text = f"Banning group {group_id} for {ban_duration} days for the reason {ban_reason}."
 
         try:
             await c.send_message(
-                user_id,
+                group_id,
                 f"You are Banned 🚫 to use this bot for **{ban_duration}** day(s) for the reason __{ban_reason}__ \n\n**Message from the admin 🤠**",
             )
-            ban_log_text += "\n\nUser notified successfully!"
+            ban_log_text += "\n\nGroup notified successfully!"
         except BaseException:
             traceback.print_exc()
             ban_log_text += (
-                f"\n\n ⚠️ User notification failed! ⚠️ \n\n`{traceback.format_exc()}`"
+                f"\n\n ⚠️ Group notification failed! ⚠️ \n\n`{traceback.format_exc()}`"
             )
-        await db.ban_user(user_id, ban_duration, ban_reason)
+        await db.ban_group(group_id, ban_duration, ban_reason)
         print(ban_log_text)
         await m.reply_text(ban_log_text, quote=True)
     except BaseException:
@@ -139,31 +139,31 @@ async def ban(c, m):
         )
 
 
-@Bot.on_message(filters.private & filters.command("unban_user"))
+@Bot.on_message(filters.private & filters.command("unban_group"))
 async def unban(c, m):
-    if m.from_user.id not in AUTH_USERS:
+    if m.from_group.id not in AUTH_USERS:
         await m.delete()
         return
     if len(m.command) == 1:
         await m.reply_text(
-            f"Use this command to unban 😃 any user.\n\nUsage:\n\n`/unban_user user_id`\n\nEg: `/unban_user 1234567`\n This will unban user with id `1234567`.",
+            f"Use this command to unban 😃 any group.\n\nUsage:\n\n`/unban_group group_id`\n\nEg: `/unban_group 1234567`\n This will unban group with id `1234567`.",
             quote=True,
         )
         return
 
     try:
-        user_id = int(m.command[1])
-        unban_log_text = f"Unbanning user 🤪 {user_id}"
+        group_id = int(m.command[1])
+        unban_log_text = f"Unbanning group 🤪 {group_id}"
 
         try:
-            await c.send_message(user_id, f"Your ban was lifted!")
-            unban_log_text += "\n\n✅ User notified successfully! ✅"
+            await c.send_message(group_id, f"Your ban was lifted!")
+            unban_log_text += "\n\n✅ Group notified successfully! ✅"
         except BaseException:
             traceback.print_exc()
             unban_log_text += (
-                f"\n\n⚠️ User notification failed! ⚠️\n\n`{traceback.format_exc()}`"
+                f"\n\n⚠️ Group notification failed! ⚠️\n\n`{traceback.format_exc()}`"
             )
-        await db.remove_ban(user_id)
+        await db.remove_ban(group_id)
         print(unban_log_text)
         await m.reply_text(unban_log_text, quote=True)
     except BaseException:
@@ -174,47 +174,47 @@ async def unban(c, m):
         )
 
 
-@Bot.on_message(filters.private & filters.command("banned_users"))
+@Bot.on_message(filters.private & filters.command("banned_groups"))
 async def _banned_usrs(c, m):
-    if m.from_user.id not in AUTH_USERS:
+    if m.from_group.id not in AUTH_USERS:
         await m.delete()
         return
-    all_banned_users = await db.get_all_banned_users()
+    all_banned_groups = await db.get_all_banned_groups()
     banned_usr_count = 0
     text = ""
-    async for banned_user in all_banned_users:
-        user_id = banned_user["id"]
-        ban_duration = banned_user["ban_status"]["ban_duration"]
-        banned_on = banned_user["ban_status"]["banned_on"]
-        ban_reason = banned_user["ban_status"]["ban_reason"]
+    async for banned_group in all_banned_groups:
+        group_id = banned_group["id"]
+        ban_duration = banned_group["ban_status"]["ban_duration"]
+        banned_on = banned_group["ban_status"]["banned_on"]
+        ban_reason = banned_group["ban_status"]["ban_reason"]
         banned_usr_count += 1
-        text += f"> **User_id**: `{user_id}`, **Ban Duration**: `{ban_duration}`, **Banned on**: `{banned_on}`, **Reason**: `{ban_reason}`\n\n"
-    reply_text = f"Total banned user(s) 🤭: `{banned_usr_count}`\n\n{text}"
+        text += f"> **Group_id**: `{group_id}`, **Ban Duration**: `{ban_duration}`, **Banned on**: `{banned_on}`, **Reason**: `{ban_reason}`\n\n"
+    reply_text = f"Total banned group(s) 🤭: `{banned_usr_count}`\n\n{text}"
     if len(reply_text) > 4096:
-        with open("banned-users.txt", "w") as f:
+        with open("banned-groups.txt", "w") as f:
             f.write(reply_text)
-        await m.reply_document("banned-users.txt", True)
-        os.remove("banned-users.txt")
+        await m.reply_document("banned-groups.txt", True)
+        os.remove("banned-groups.txt")
         return
     await m.reply_text(reply_text, True)
 
 
 @Bot.on_callback_query()
 async def callback_handlers(bot: Client, cb: CallbackQuery):
-    user_id = cb.from_user.id
+    group_id = cb.from_group.id
     if cb.data == "notifon":
-        notif = await db.get_notif(cb.from_user.id)
+        notif = await db.get_notif(cb.from_group.id)
         if notif is True:
-            await db.set_notif(user_id, notif=False)
+            await db.set_notif(group_id, notif=False)
         else:
-            await db.set_notif(user_id, notif=True)
+            await db.set_notif(group_id, notif=True)
         await cb.message.edit(
-            f"`Here You Can Set Your Settings:`\n\nSuccessfully setted notifications to **{await db.get_notif(user_id)}**",
+            f"`Here You Can Set Your Settings:`\n\nSuccessfully setted notifications to **{await db.get_notif(group_id)}**",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
                         InlineKeyboardButton(
-                            f"NOTIFICATION  {'🔔' if ((await db.get_notif(user_id)) is True) else '🔕'}",
+                            f"NOTIFICATION  {'🔔' if ((await db.get_notif(group_id)) is True) else '🔕'}",
                             callback_data="notifon",
                         )
                     ],
@@ -223,7 +223,7 @@ async def callback_handlers(bot: Client, cb: CallbackQuery):
             ),
         )
         await cb.answer(
-            f"Successfully setted notifications to {await db.get_notif(user_id)}"
+            f"Successfully setted notifications to {await db.get_notif(group_id)}"
         )
     else:
         await cb.message.delete(True)
