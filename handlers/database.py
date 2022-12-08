81641ce2d4d3b0d031e98c026490e56793b56481
Emir <marveldarkbull@gmail.com>
2022-12-08 19:58:31 +0300

    Update database.py

--- handlers/database.py
+++ handlers/database.py
@@ -9,9 +9,9 @@ class Database:
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
-        self.col = self.db.users
+        self.col = self.db.groups
 
-    def new_user(self, id):
+    def new_group(self, id):
         return dict(
             id=id,
             join_date=datetime.date.today().isoformat(),
@@ -24,24 +24,24 @@ class Database:
             ),
         )
 
-    async def add_user(self, id):
-        user = self.new_user(id)
-        await self.col.insert_one(user)
+    async def add_group(self, id):
+        group = self.new_group(id)
+        await self.col.insert_one(group)
 
-    async def is_user_exist(self, id):
-        user = await self.col.find_one({"id": int(id)})
-        return True if user else False
+    async def is_group_exist(self, id):
+        group = await self.col.find_one({"id": int(id)})
+        return True if group else False
 
-    async def total_users_count(self):
+    async def total_groups_count(self):
         count = await self.col.count_documents({})
         return count
 
-    async def get_all_users(self):
-        all_users = self.col.find({})
-        return all_users
+    async def get_all_groups(self):
+        all_groups = self.col.find({})
+        return all_groups
 
-    async def delete_user(self, user_id):
-        await self.col.delete_many({"id": int(user_id)})
+    async def delete_group(self, group_id):
+        await self.col.delete_many({"id": int(group_id)})
 
     async def remove_ban(self, id):
         ban_status = dict(
@@ -52,14 +52,14 @@ class Database:
         )
         await self.col.update_one({"id": id}, {"$set": {"ban_status": ban_status}})
 
-    async def ban_user(self, user_id, ban_duration, ban_reason):
+    async def ban_group(self, group_id, ban_duration, ban_reason):
         ban_status = dict(
             is_banned=True,
             ban_duration=ban_duration,
             banned_on=datetime.date.today().isoformat(),
             ban_reason=ban_reason,
         )
-        await self.col.update_one({"id": user_id}, {"$set": {"ban_status": ban_status}})
+        await self.col.update_one({"id": group_id}, {"$set": {"ban_status": ban_status}})
 
     async def get_ban_status(self, id):
         default = dict(
@@ -68,24 +68,24 @@ class Database:
             banned_on=datetime.date.max.isoformat(),
             ban_reason="",
         )
-        user = await self.col.find_one({"id": int(id)})
-        return user.get("ban_status", default)
+        group = await self.col.find_one({"id": int(id)})
+        return group.get("ban_status", default)
 
-    async def get_all_banned_users(self):
-        banned_users = self.col.find({"ban_status.is_banned": True})
-        return banned_users
+    async def get_all_banned_groups(self):
+        banned_groups = self.col.find({"ban_status.is_banned": True})
+        return banned_groups
 
     async def set_notif(self, id, notif):
         await self.col.update_one({"id": id}, {"$set": {"notif": notif}})
 
     async def get_notif(self, id):
-        user = await self.col.find_one({"id": int(id)})
-        return user.get("notif", False)
+        group = await self.col.find_one({"id": int(id)})
+        return group.get("notif", False)
 
-    async def get_all_notif_user(self):
-        notif_users = self.col.find({"notif": True})
-        return notif_users
+    async def get_all_notif_group(self):
+        notif_groups = self.col.find({"notif": True})
+        return notif_groups
 
-    async def total_notif_users_count(self):
+    async def total_notif_groups_count(self):
         count = await self.col.count_documents({"notif": True})
         return count
