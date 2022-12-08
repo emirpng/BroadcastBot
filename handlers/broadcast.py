2391b96c1439e90eef7cb11fb9d9315ad6432990
Emir <marveldarkbull@gmail.com>
2022-12-08 19:59:56 +0300

    Update broadcast.py

--- handlers/broadcast.py
+++ handlers/broadcast.py
@@ -11,9 +11,9 @@ import traceback
 import aiofiles
 from pyrogram.errors import (
     FloodWait,
-    InputUserDeactivated,
+    InputGroupDeactivated,
     PeerIdInvalid,
-    UserIsBlocked,
+    GroupIsBlocked,
 )
 
 import config
@@ -23,47 +23,47 @@ broadcast_ids = {}
 BROADCAST_AS_COPY = config.BROADCAST_AS_COPY
 
 
-async def send_msg(user_id, message):
+async def send_msg(group_id, message):
     try:
         if BROADCAST_AS_COPY is False:
-            await message.forward(chat_id=user_id)
+            await message.forward(chat_id=group_id)
         elif BROADCAST_AS_COPY is True:
-            await message.copy(chat_id=user_id)
+            await message.copy(chat_id=group_id)
         return 200, None
     except FloodWait as e:
         await asyncio.sleep(e.x)
-        return send_msg(user_id, message)
-    except InputUserDeactivated:
-        return 400, f"{user_id} : deactivated\n"
-    except UserIsBlocked:
-        return 400, f"{user_id} : blocked the bot\n"
+        return send_msg(group_id, message)
+    except InputGroupDeactivated:
+        return 400, f"{group_id} : deactivated\n"
+    except GroupIsBlocked:
+        return 400, f"{group_id} : blocked the bot\n"
     except PeerIdInvalid:
-        return 400, f"{user_id} : user id invalid\n"
+        return 400, f"{group_id} : group id invalid\n"
     except Exception:
-        return 500, f"{user_id} : {traceback.format_exc()}\n"
+        return 500, f"{group_id} : {traceback.format_exc()}\n"
 
 
 async def broadcast(m, db):
-    all_users = await db.get_all_notif_user()
+    all_groups = await db.get_all_notif_group()
     broadcast_msg = m.reply_to_message
     while True:
         broadcast_id = "".join([random.choice(string.ascii_letters) for i in range(3)])
         if not broadcast_ids.get(broadcast_id):
             break
     out = await m.reply_text(
-        text=f"Broadcast Started! You will be notified with log file when all the users are notified."
+        text=f"Broadcast Started! You will be notified with log file when all the groups are notified."
     )
     start_time = time.time()
-    total_users = await db.total_users_count()
+    total_groups = await db.total_groups_count()
     done = 0
     failed = 0
     success = 0
     broadcast_ids[broadcast_id] = dict(
-        total=total_users, current=done, failed=failed, success=success
+        total=total_groups, current=done, failed=failed, success=success
     )
     async with aiofiles.open("broadcast.txt", "w") as broadcast_log_file:
-        async for user in all_users:
-            sts, msg = await send_msg(user_id=int(user["id"]), message=broadcast_msg)
+        async for group in all_groups:
+            sts, msg = await send_msg(group_id=int(group["id"]), message=broadcast_msg)
             if msg is not None:
                 await broadcast_log_file.write(msg)
             if sts == 200:
@@ -71,7 +71,7 @@ async def broadcast(m, db):
             else:
                 failed += 1
             if sts == 400:
-                await db.delete_user(user["id"])
+                await db.delete_group(group["id"])
             done += 1
             if broadcast_ids.get(broadcast_id) is None:
                 break
@@ -86,13 +86,13 @@ async def broadcast(m, db):
     await out.delete()
     if failed == 0:
         await m.reply_text(
-            text=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
+            text=f"broadcast completed in `{completed_in}`\n\nTotal groups {total_groups}.\nTotal done {done}, {success} success and {failed} failed.",
             quote=True,
         )
     else:
         await m.reply_document(
             document="broadcast.txt",
-            caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
+            caption=f"broadcast completed in `{completed_in}`\n\nTotal groups {total_groups}.\nTotal done {done}, {success} success and {failed} failed.",
             quote=True,
         )
     os.remove("broadcast.txt")
